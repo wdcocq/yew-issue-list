78ce952bf85523e24fd47e8c18acbfba8a658bb8
Wester de Cocq <w.decocq@gmail.com>
2022-04-19 19:05:26 +0200

    Commit reproducible issue with suspended list

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,14 @@
+use log::info;
 use yew::prelude::*;
 use yew_macro::Properties;
 
-#[cfg(not(target_arch = "wasm32"))]
+// #[cfg(not(target_arch = "wasm32"))]
 #[function_component(App)]
 pub fn app() -> Html {
-    let size = use_state(|| 4);
+    let size = use_state(|| 10);
 
     html! {
         <div>
-            <List size={*size}/>
-        </div>
-    }
-}
-
-#[cfg(target_arch = "wasm32")]
-#[function_component(App)]
-pub fn app() -> Html {
-    use yew::callback;
-
-    let size = use_state(|| 4);
-
-    let callback = {
-        let size = size.clone();
-        Callback::from(move |_| {
-            size.set(10);
-        })
-    };
-
-    html! {
-        <div onclick={callback}>
             <List size={*size}/>
         </div>
     }
@@ -45,25 +25,84 @@ fn list(props: &ListProps) -> Html {
 
     html! {
         <>
-        { for elems.clone().map(|_|
+        { for elems.clone().map(|number|
             html! {
-                <Test2/> // fails
-                // <Test1> // works
+                <ToSuspendOrNot {number}/>
             }
         )}
         </>
     }
 }
 
-#[function_component(Test1)]
-fn test1() -> Html {
+#[derive(Properties, PartialEq)]
+struct NumberProps {
+    number: u32,
+}
+
+#[function_component(Number)]
+fn number(props: &NumberProps) -> Html {
     html! {
-        <span>{"test"}</span>
+        <div>{props.number.to_string()}</div>
     }
 }
-#[function_component(Test2)]
-fn test2() -> Html {
+#[function_component(SuspendedNumber)]
+fn suspended_number(props: &NumberProps) -> HtmlResult {
+    sleep::use_sleep()?;
+    Ok(html! {
+        <div>{props.number.to_string()}</div>
+    })
+}
+#[function_component(ToSuspendOrNot)]
+fn suspended_number(props: &NumberProps) -> Html {
+    let number = props.number;
     html! {
-        <Test1/>
+        <Suspense>
+            if number % 3 == 0 {
+                <SuspendedNumber {number}/>
+            } else {
+                <Number {number}/>
+            }
+        </Suspense>
+    }
+}
+
+mod sleep {
+    use std::rc::Rc;
+    use yew::prelude::*;
+    use yew::suspense::{Suspension, SuspensionResult};
+
+    #[derive(PartialEq)]
+    pub struct SleepState {
+        s: Suspension,
+    }
+
+    impl SleepState {
+        fn new() -> Self {
+            let s = Suspension::from_future(async {
+                #[cfg(target_arch = "wasm32")]
+                gloo_timers::future::sleep(std::time::Duration::from_secs(1)).await;
+            });
+
+            Self { s }
+        }
+    }
+
+    impl Reducible for SleepState {
+        type Action = ();
+
+        fn reduce(self: Rc<Self>, _action: Self::Action) -> Rc<Self> {
+            Self::new().into()
+        }
+    }
+
+    #[hook]
+    pub fn use_sleep() -> SuspensionResult<Rc<dyn Fn()>> {
+        let sleep_state = use_reducer(SleepState::new);
+
+        if sleep_state.s.resumed() {
+            Ok(Rc::new(move || sleep_state.dispatch(())))
+        } else {
+            Err(sleep_state.s.clone())
+        }
     }
 }
